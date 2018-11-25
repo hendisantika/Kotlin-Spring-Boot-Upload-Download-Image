a715b739aea4772c053aa3206eaf758973d6da65
Hendi Santika <hendisantika@gmail.com>
2018-11-26 04:02:41 +0700

    #11 - Modify Main App

--- src/main/kotlin/com/hendisantika/kotlinspringbootuploaddownloadimage/KotlinSpringBootUploadDownloadImageApplication.kt
+++ src/main/kotlin/com/hendisantika/kotlinspringbootuploaddownloadimage/KotlinSpringBootUploadDownloadImageApplication.kt
@@ -1,10 +1,23 @@
 package com.hendisantika.kotlinspringbootuploaddownloadimage
 
+import com.hendisantika.kotlinspringbootuploaddownloadimage.service.FileStorage
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class KotlinSpringBootUploadDownloadImageApplication
+class KotlinSpringBootUploadDownloadImageApplication {
+    @Autowired
+    lateinit var fileStorage: FileStorage
+
+    @Bean
+    fun run() = CommandLineRunner {
+        fileStorage.deleteAll()
+        fileStorage.init()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KotlinSpringBootUploadDownloadImageApplication>(*args)
