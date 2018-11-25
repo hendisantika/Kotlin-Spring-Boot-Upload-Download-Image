895670b52f2a818920c92af072575183be9c3ee5
Hendi Santika <hendisantika@gmail.com>
2018-11-26 03:52:17 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/kotlinspringbootuploaddownloadimage/KotlinSpringBootUploadDownloadImageApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.kotlinspringbootuploaddownloadimage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootUploadDownloadImageApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootUploadDownloadImageApplication>(*args)
+}
