56167736564e4fa896ca994835720a3148afd211
Hendi Santika <hendisantika@gmail.com>
2018-11-26 03:56:27 +0700

    #5 - Add File Info Model

---
+++ src/main/kotlin/com/hendisantika/kotlinspringbootuploaddownloadimage/model/FileInfo.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.kotlinspringbootuploaddownloadimage.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-spring-boot-upload-download-image
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-11-26
+ * Time: 03:56
+ * To change this template use File | Settings | File Templates.
+ */
+class FileInfo(val filename: String = "", val url: String = "")
